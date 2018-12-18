4284613489c6890b313ba3e7d800111f31dcbb0d
stefansilverio <494@holbertonschool.com>
2018-12-18 21:42:03 +0000

    new main file

--- main.c
+++ main.c
@@ -3,21 +3,39 @@
 /**
  *
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	stack_t *result = NULL;
-	stack_t *head = NULL;
+	stack_t *result = NULL, *head = NULL;
+	char *buffer = NULL;
+	FILE *fp;
+	size_t count = 0, n;
 
-	result = add_node_start(&head, 10);
-	result = add_node_start(&head, 20);
-	result = add_node_start(&head, 30);
-	result = add_node_start(&head, 40);
-	result = add_node_start(&head, 50);
+	result = build_list(&head);
 
 	while (result->next)
 	{
 		printf("%d\n", result->n);
 		result = result->next;
 	}
+
+	fp = fopen(const char *path, const char r+);
+/* fopen returns null on failure */
+
+	count = getline(&buffer, &n, fp);
+/* check count to filter out failures */
+	while (count != -1)
+	{
+		count = getline(&buffer, &n, fp);
+		result = tokenize(result, &buffer); /* result is at top of list */
+	}
+
+	while (result->next)
+	{
+		printf("%d\n", result->n);
+		result = result->next;
+	}
+
+	fclose(fp);
+
 	return (0);
 }
