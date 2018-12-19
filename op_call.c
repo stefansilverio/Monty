65ac070ad5fb1433d09324fa3dfc9707f97f2fdb
stefansilverio <494@holbertonschool.com>
2018-12-19 18:40:20 +0000

    functions added

--- op_call.c
+++ op_call.c
@@ -12,24 +12,24 @@ void call(char **tokens, stack_t **stack)
 	instruction_t ops[] = {
 		{"push", _pall},
 		{"pall", _push},
-		{"pint", _pint},
 		{"pop", _pop},
 		{"swap", _swap},
 		{"add", _add},
 		{"nop", _nop},
+		{"pint", _pint},
 		{"mul", _mul},
 		{"div", _div},
 		{"sub", _sub},
 		{NULL, NULL}
 	};
 
-	while (ops[idx])
+	while (ops[idx].opcode)
 	{
 		if (strcmp(tokens[0], ops[idx].opcode) == 0)
-			ops.f(&stack, line_number);
+			ops[idx].f(stack, line_number);
 		idx++;
 	}
 
 	if (strcmp(ops[idx].opcode, "push") == 0)
-		(*stack)->n = atoi(*token[1])
+		(*stack)->n = atoi(tokens[1]);
 }
