8fb227203e77ea323e6f15205f09f98486bc725b
stefansilverio <494@holbertonschool.com>
2018-12-19 18:31:58 +0000

    new main files

--- main.c
+++ main.c
@@ -7,56 +7,28 @@ unsigned int line_number = 0;
  */
 int main(int argc, char *argv[])
 {
-	char **result = NULL;
-	stack_t *head = NULL;
-	char *buffer = NULL;
+	char **tokens = NULL; /* for tokenized list */
+	stack_t *head = NULL; /* pointer to top of stack */
+	char *buffer = NULL; /* store getline */
 	FILE *fp;
 	size_t count = 0, n;
 
-	result = build_list(&head);
+	(void) argc;
+	(void) argv;
 
-	while (result->next)
-	{
-		printf("%d\n", result->n);
-		result = result->next;
-	}
-
-	fp = fopen(const char *path, const char r+);
+	fp = fopen(argv[1], "r+");
+	if (fp == NULL)
+		puts("fail");
 /* fopen returns null on failure */
 
 /* check count to filter out failures */
-	while (getline(&buffer, &n, fp) != 0)
+	while (getline(&buffer, &n, fp) != -1)
 	{
 		count++;
-		result = tokenize(buffer); /* result is at top of list */
-		call(result, &head);
+		tokens = tokenize(buffer); /* result is at top of list */
+		call(tokens, &head);
 	}
-
-	while (result->next)
-	{
-		printf("%d\n", result->n);
-		result = result->next;
-	}
-
 	fclose(fp);
 
 	return (0);
-}
-#include "monty.h"
-
-/**
- *
- */
-stack_t *build_list(stack_t **head)
-{
-	stack_t *result = NULL;
-
-	result = add_node_start(head, 10);
-	result = add_node_start(head, 20);
-	result = add_node_start(head, 30);
-	result = add_node_start(head, 40);
-	result = add_node_start(head, 50);
-	result = add_node_start(head, 60);
-
-	return (result);
 }
