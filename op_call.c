3cd7fff4da25b41db12d87b8ae789dd3e2998992
Martin Smith <msmith3dart@icloud.com>
2018-12-19 01:34:40 +0000

    minor fix

--- op_call.c
+++ op_call.c
@@ -9,7 +9,6 @@
 void call(char **tokens, stack_t **stack)
 {
 	int idx = 0;
-	int line_number = atoi(*t[1]);
 	instruction_t ops[] = {
 		{"push", push},
 		{"pall", pall},
@@ -25,6 +24,7 @@ void call(char **tokens, stack_t **stack)
 	{
 		if (strcmp(tokens[0], ops[idx].opcode) == 0)
 			ops.f(&stack, line_number);
+		idx++;
 	}
 
 	if (strcmp(ops[idx].opcode, "push") == 0)
